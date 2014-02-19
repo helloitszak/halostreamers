69e4ccf91c50b9d65d2ea0fc6986d00277d4ccf9
Zak "Ubercow" Kristjanson <ubercow1337@gmail.com>
2014-02-18 23:39:25 -0800

    WE VERSION 1 BABY

---
+++ README.md
@@ -0,0 +1,24 @@
+# Halo Streamers
+This is a little sinatra site made for my good friend [Andrew "aWildeOne" Wilde](http://twitch.tv/awildeone) to use on Twitch.tv this is meant as a custom handler for [NiteBot](http://www.nightbot.tv/)
+
+It currently displays the live runners from [HaloRuns.com](http://haloruns.com/runners)'s streamer page and outputs a nice little formatted text listing online streamers.
+
+
+
+## I want to use this myself!
+It's pretty easy. It's a simple sinatra application. Run the bundle for the gems and the only configuration you'll need is a Twitch TV API Client Key. With a little bit of Ruby knowledge, you can also configure it to add additional streams to the Halo Streamer list, not use the halo streamer list at all.
+
+## I'm a Halo Runner and I want this on my stream!
+You're also in luck, but not right now. I'm planning on hosting this for public, that should be coming soon™.
+
+## I'm not a Halo Runner but I want something similar for my own list of streams, but I don't have the resources to set it up myself
+I have no plans for this, but if there is interest, hit me up on [Twitter](https://www.twitter.com/_ubercow) and I might be able to set something up.
+
+## Is it cool
+yes
+
+## Your program sucks and I can make it better
+Please do! If there is something I can do better, I'll happily accept pull requests.
+
+## You smell and I want to steal this for my own site
+As long as you follow the terms indicated in the LICENSE file, go right ahead.
---
+++ streamers.rb
@@ -0,0 +1,102 @@
+require 'nokogiri'
+require 'net/http'
+require 'open-uri'
+require 'json'
+require 'sinatra/base'
+
+TWITCH_CLIENT_ID = ""
+
+class Streamers
+	def self.halo_streamers
+		streamers = []
+
+		doc = Nokogiri::HTML(open("http://haloruns.com/runners"))
+		table = doc.css("#runnersTable")
+
+		twitch_index = table.css("tbody tr th").to_a.index { |x| x.text == "Twitch" }
+		table.css("tbody tr").each do |row|
+			cols = row.css("td")
+			if cols.empty?
+				next
+			end
+			
+			twitch_link = cols[twitch_index].css("a")
+
+			if not twitch_link.empty?
+				streamers << twitch_link.first.attribute("href").value
+			end
+		end
+
+		streamers.map { |x| x =~ /http:\/\/www\.twitch\.tv\/(.+)\/profile/; $1 }
+	end
+
+	def self.stream_info(users)
+		#puts "DEBUG: Info for #{users.join(", ")}"
+		http = Net::HTTP.new("api.twitch.tv", 443)
+		http.use_ssl = true
+		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+		if users.is_a? Array
+			users = users.join(",")
+		end
+
+		request = Net::HTTP::Get.new("/kraken/streams?channel=#{users}")
+
+		request.initialize_http_header({
+			"Accept" => "application/vnd.twitchtv.v3+json",
+			"Client-ID" => TWITCH_CLIENT_ID
+		})
+
+		response = http.request(request)
+
+		unless response.kind_of?(Net::HTTPSuccess)
+			return []
+		end
+
+		rj = JSON.parse(response.body)
+
+		rj["streams"].map do |stream|
+			{ :name => stream["channel"]["display_name"], :viewers => stream["viewers"]}
+		end
+	end
+
+	def self.streamers_list
+		### CONFIGURATION STARTS HERE ###
+		streamers = []
+
+		# Halo Streamers
+		streamers.push(*Streamers.halo_streamers)
+
+		# Add additional streamers here (shameless self plug)
+		streamers.push("chaoxlol")
+		streamers.push("andy")
+		### CONFIGURATION ENEDS HERE ###
+
+
+		Streamers.stream_info(streamers)
+	end
+end
+
+class Webapp < Sinatra::Base
+	get '/' do
+		streamers = Streamers.streamers_list
+		top = 5
+
+		if params['top']
+			top = params['top'].to_i
+		end
+
+		if params['sort'] == 'asc'
+			# lowest to highest
+			streamers.sort! { |x,y| x[:viewers] <=> y[:viewers] }
+		else
+			# highest to lowest
+			streamers.sort! { |x,y| y[:viewers] <=> x[:viewers] }
+		end
+
+		streamers[0, top]
+			.each_with_index
+			.map { |s,i| "#{i+1}: #{s[:name]}" }
+			.join(" ")
+	end
+end
